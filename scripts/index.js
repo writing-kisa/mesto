d52c814ca3b2d3d52e994372ca1f89f70e1ed706
vika <ms.victoryart@gmail.com>
2023-02-18 14:13:54 +0400

    в работе 5-пр: на страницу выводятся карточки из массива (с помощью темплейта и джава скрипта), а также сделала новый попап для добавления новой карточки: открывается нажатием на соотв. кнопку и закрывается с помощью крестика

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const buttonEditName = document.querySelector(".profile__button-change-name");
 const popupEditName = document.querySelector(".popup");
 // console.log(popupEditName);
 const popupEditCloseButton = document.querySelector("#name_close_button");
-// console.log(popupEditCloseButton);
+// console.log(popupCloseButton);
 const formElement = document.querySelector("#submit_name_form");
 const nameInput = formElement.querySelector("#text-name");
 // console.log(nameInput.value);
@@ -11,27 +11,81 @@ const bioInput = formElement.querySelector("#text-bio");
 // console.log(bioInput.value);
 const profileName = document.querySelector(".profile__name");
 const profileBio = document.querySelector(".profile__bio");
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+const cardContainer = document.querySelector('#card-container');
+const buttonAddCard = document.querySelector('.profile__button-add-photo');
+const popupAddCard = document.querySelector('#add_card');
+const popupAddCardCloseButton = document.querySelector("#new-card_close_button");
 
 
-function openPopup() {
-    popupEditName.classList.add("popup_opened");
-    nameInput.value = profileName.textContent;
-    bioInput.value = profileBio.textContent;
+// console.log(initialCards[0].name);
+
+function openPopup(evt) {
+  evt.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  bioInput.value = profileBio.textContent;
 }
 
-function closePopup() {
-    popupEditName.classList.remove("popup_opened");
+function closePopup(evt) {
+  evt.classList.remove("popup_opened");
 }
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
-    profileName.textContent = nameInput.value;
-    profileBio.textContent = bioInput.value;
-    
-    closePopup(popupEditName);
+  profileName.textContent = nameInput.value;
+  profileBio.textContent = bioInput.value;
+
+  closePopup(popupEditName);
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
-buttonEditName.addEventListener('click', () => openPopup());
-popupEditCloseButton.addEventListener('click', () => closePopup());
+function createCard(card) {
+
+  const cardTemplate = document.querySelector("#cards").content;
+  const cardElement = cardTemplate.querySelector(".gallery__cell").cloneNode(true);
+  const cardName = cardElement.querySelector(".gallery__name");
+  const cardLink = cardElement.querySelector(".gallery__photo");
+
+// console.log(cardTemplate);
+// console.log(cardElement);
+// console.log(cardName.textContent);
+// console.log(cardLink.src);
+
+    cardName.textContent = card.name;
+    cardLink.src = card.link;
+
+    cardContainer.append(cardElement);
+}
+
+initialCards.forEach(createCard);
+
+formElement.addEventListener("submit", handleFormSubmit);
+buttonEditName.addEventListener("click", () => openPopup(popupEditName));
+popupEditCloseButton.addEventListener("click", () => closePopup(popupEditName));
+buttonAddCard.addEventListener("click", () => openPopup(popupAddCard));
+popupAddCardCloseButton.addEventListener("click", () => closePopup(popupAddCard));
