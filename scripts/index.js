bf7e246c2e12607369f00b4f42254e34e05292e4
vika <ms.victoryart@gmail.com>
2023-02-20 17:31:31 +0400

    сделала возможность добавления карточки на страницу через форму

--- scripts/index.js
+++ scripts/index.js
@@ -37,13 +37,18 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-const cardContainer = document.querySelector('#card-container');
-const buttonAddCard = document.querySelector('.profile__button-add-photo');
-const popupAddCard = document.querySelector('#add_card');
-const popupAddCardCloseButton = document.querySelector("#new-card_close_button");
-
-
 // console.log(initialCards[0].name);
+
+const cardContainer = document.querySelector("#card-container");
+const buttonAddCard = document.querySelector(".profile__button-add-photo");
+const popupAddCard = document.querySelector("#add_card");
+const popupAddCardCloseButton = document.querySelector(
+  "#new-card_close_button"
+);
+const addCardSaveButton = document.querySelector("#new-card_save_button");
+const newCardForm = document.querySelector("#submit_new-card_form");
+const cardNameInput = newCardForm.querySelector("#card-name");
+const cardLinkInput = newCardForm.querySelector("#card-link");
 
 function openPopup(evt) {
   evt.classList.add("popup_opened");
@@ -64,28 +69,36 @@ function handleFormSubmit(evt) {
   closePopup(popupEditName);
 }
 
-function createCard(card) {
-
+function createCard(name, link) {
   const cardTemplate = document.querySelector("#cards").content;
-  const cardElement = cardTemplate.querySelector(".gallery__cell").cloneNode(true);
+  const cardElement = cardTemplate
+    .querySelector(".gallery__cell")
+    .cloneNode(true);
   const cardName = cardElement.querySelector(".gallery__name");
   const cardLink = cardElement.querySelector(".gallery__photo");
+  // console.log(cardTemplate);
+  // console.log(cardElement);
+  // console.log(cardName.textContent);
+  // console.log(cardLink.src);
 
-// console.log(cardTemplate);
-// console.log(cardElement);
-// console.log(cardName.textContent);
-// console.log(cardLink.src);
+  cardName.textContent = name;
+  cardLink.src = link;
 
-    cardName.textContent = card.name;
-    cardLink.src = card.link;
-
-    cardContainer.append(cardElement);
+  cardContainer.prepend(cardElement);
 }
 
-initialCards.forEach(createCard);
+initialCards.forEach((card) => createCard(card.name, card.link));
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  createCard(cardNameInput.value, cardLinkInput.value);
+  closePopup(popupAddCard);
+}
 
 formElement.addEventListener("submit", handleFormSubmit);
 buttonEditName.addEventListener("click", () => openPopup(popupEditName));
 popupEditCloseButton.addEventListener("click", () => closePopup(popupEditName));
 buttonAddCard.addEventListener("click", () => openPopup(popupAddCard));
 popupAddCardCloseButton.addEventListener("click", () => closePopup(popupAddCard));
+newCardForm.addEventListener("submit", handleAddCardSubmit);
+
